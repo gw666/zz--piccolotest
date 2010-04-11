e457f75546d9ab15efa31a0588d8bdda4cc20fad
Gregg Williams <gregg13@innerpaths.net>
2010-04-10 20:35:56 -0700

    debugging ToggleShape; all previous to this works

--- src/piccolotest.clj
+++ src/piccolotest.clj
@@ -8,20 +8,29 @@
   (:gen-class)
   (:import
     (java.awt   Color Graphics2D)
+    (edu.umd.cs.piccolo.nodes   PHtmlView PImage PPath PText)
     (edu.umd.cs.piccolo   PCamera PCanvas PInputManager PLayer PNode
       POffscreenCanvas PRoot)
     (edu.umd.cs.piccolo.event   PBasicInputEventHandler PDragEventHandler
       PDragSequenceEventHandler PInputEvent PInputEventFilter PPanEventHandler
       PZoomEventHandler)
-    (edu.umd.cs.piccolo.nodes   PHtmlView PImage PPath PText)
     (edu.umd.cs.piccolo.util   PAffineTransform PBounds PDebug PDimension
       PObjectOutputStream PPaintContext PPickPath PStack PUtil)
-    (edu.umd.cs.piccolox   PApplet PFrame)
-    ))
+    (edu.umd.cs.piccolox   PApplet PFrame)))
 
 (defn create-interface-frame
   "Creates the main InterfaceFrame used by the program."
   []
+
+  (defn create-toggle-shape
+    "Creates an ellipse that changes color when it is clicked."
+    []
+    (proxy [PPath] []
+      (PPath []
+        (.setPathToEllipse this 0 0 100 80)
+        (println "Hello world!"))))
+
+
   (proxy [PFrame] []
     (initialize []
       (let [aNode (PNode.)
@@ -43,7 +52,7 @@
         (.setBounds aNode -10 -10 200 110)
 
         (.translate aNode 100 100)
-        (.scale aNode (Float. 1.5))
+        (.scale aNode 1.5)
         (.rotate aNode 45)
 
         (.addChild layer (PPath/createEllipse 0 0 100 100))
@@ -53,7 +62,38 @@
         (let [image (PImage. (.toImage layer 300 300 nil))]
           (.addChild layer image))
 
-        ))))
+        (let [myCompositeFace (PPath/createRectangle 0 0 100 80)
+              eye1 (PPath/createEllipse 0 0 20 20)
+              eye2 (.clone eye1)
+              mouth (PPath/createRectangle 0 0 40 20)
+              ]
+          (.setPaint eye1 (Color/YELLOW))
+          (.setPaint eye2 (Color/YELLOW)) ; "let" forces deviation from original
+          (.setPaint mouth (Color/BLACK))
+          (.addChild myCompositeFace eye1)
+          (.addChild myCompositeFace eye2)
+          (.addChild myCompositeFace mouth)
+
+          (.setChildrenPickable myCompositeFace false)
+
+          (.translate eye2 25 0)
+          (.translate mouth 0 30)
+
+          (let [b (.getUnionOfChildrenBounds myCompositeFace nil)]
+            (.inset b -5 -5)
+            (.setBounds myCompositeFace b)
+
+            (.scale myCompositeFace 1.5)
+            
+            (.addChild layer myCompositeFace))
+
+          (let [ts (create-toggle-shape)]
+            (.setPaint ts (Color/ORANGE))
+            (.addChild layer ts))
+          )))))
+
+
+
 
 (defn -main []
   (let [main-frame (create-interface-frame)]
